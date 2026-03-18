465e86c0ee162e35ee6c21ddfd116bf97ae3f515
Максим Гулько <gulashmail@gmail.com>
2026-03-18 22:39:25 +0300

    Working versions

---
+++ convention-plugin-buildsrc/buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal() // so that external plugins can be resolved in dependencies section
+}
+
+dependencies {
+    implementation("com.github.spotbugs:com.github.spotbugs.gradle.plugin:6.4.8")
+    testImplementation("junit:junit:4.13")
+}
